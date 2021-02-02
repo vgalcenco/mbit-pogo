83a744405aaf1a51b5be2ab5d95d041cf0542c36
Vladimir Galcenco <vgalcenco@gmail.com>
2021-02-02 23:17:39 +0000

    Decrease acceleration limit to make the counter more sensitive

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-function init () {
+function reset () {
     jumps = -1
     basic.clearScreen()
 }
 let acc = 0
 let jumps = 0
-init()
-input.onButtonPressed(Button.A, init)
+input.onButtonPressed(Button.A, reset)
+reset()
 basic.forever(function () {
     acc = input.acceleration(Dimension.Y)
-    if (acc > 2000) {
+    if (acc > 1500) {
         jumps += 1
         led.plot(jumps % 5, jumps / 5)
-        basic.pause(50)
+        basic.pause(80)
     }
 })

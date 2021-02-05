050aaa864673be3b5680d6309dc7ff39a106fc63
Vladimir Galcenco <vgalcenco@gmail.com>
2021-02-05 16:32:12 +0000

    Integrated TM1637 display :thumbs_up:

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+def on_button_pressed_a():
+    global accThreshold
+    accThreshold += 50
+    tm.show_number(accThreshold)
+    basic.pause(100)
+    tm.show_number(jumps)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_button_pressed_b():
+    global accThreshold
+    accThreshold += -50
+    tm.show_number(accThreshold)
+    basic.pause(100)
+    tm.show_number(jumps)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def reset():
+    global jumps, tm
+    jumps = 0
+    tm = TM1637.create(DigitalPin.P0, DigitalPin.P1, 7, 4)
+acc = 0
+jumps = 0
+tm: TM1637.TM1637LEDs = None
+accThreshold = 2000
+input.on_button_pressed(Button.AB, reset)
+reset()
+
+def on_forever():
+    global acc, jumps
+    acc = input.acceleration(Dimension.Y)
+    if acc > accThreshold:
+        jumps += 1
+        tm.show_number(jumps)
+        basic.pause(150)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,16 +1,37 @@
+input.onButtonPressed(Button.A, function () {
+    accThreshold += 50
+    tm.showNumber(accThreshold)
+    basic.pause(500)
+    tm.showNumber(jumps)
+})
+input.onButtonPressed(Button.B, function () {
+    accThreshold += -50
+    tm.showNumber(accThreshold)
+    basic.pause(500)
+    tm.showNumber(jumps)
+})
 function reset () {
-    jumps = -1
-    basic.clearScreen()
+    jumps = 0
+    tm.showNumber(jumps)
 }
 let acc = 0
 let jumps = 0
-input.onButtonPressed(Button.A, reset)
+let accThreshold = 0
+let tm: TM1637.TM1637LEDs = null
+tm = TM1637.create(
+DigitalPin.P0,
+DigitalPin.P1,
+7,
+4
+)
+accThreshold = 2000
+input.onButtonPressed(Button.AB, reset)
 reset()
 basic.forever(function () {
     acc = input.acceleration(Dimension.Y)
-    if (acc > 1500) {
+    if (acc > accThreshold) {
         jumps += 1
-        led.plot(jumps % 5, jumps / 5)
-        basic.pause(80)
+        tm.showNumber(jumps)
+        basic.pause(150)
     }
 })
